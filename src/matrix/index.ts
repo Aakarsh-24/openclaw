b80481a007cba2de17bb1210b1bc600f4b4ae7f1
Sebastian Schubotz <git@sibbl.net>
2026-01-11 08:43:04 +0100

    feat(matrix): refactor e2ee verification flow

--- src/matrix/index.ts
+++ src/matrix/index.ts
@@ -2,3 +2,12 @@ export * from "./actions.js";
 export { monitorMatrixProvider } from "./monitor.js";
 export { probeMatrix } from "./probe.js";
 export { sendMessageMatrix, sendPollMatrix } from "./send.js";
+export {
+  formatSasForDisplay,
+  getDeviceVerificationStatus,
+  waitForVerificationRequest,
+  type SasEmoji,
+  type SasShowData,
+  type VerificationOpts,
+  type VerificationResult,
+} from "./verification.js";
